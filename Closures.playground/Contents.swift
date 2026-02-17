549a06e9cc086e4622d3f334911f60c6dd4cfb26
AtesiT <atesit@mail.ru>
2026-02-17 17:42:30 +0300

    ADD: Closures (2 funcs where wordCompletion: (String) and wordCompletion: @escaping (String))

--- Closures.playground/Contents.swift
+++ Closures.playground/Contents.swift
@@ -38,3 +38,49 @@ wordPeckerSplit(phrase: "Word Pecker") {
     print("Split")
 }
 
+//  MARK: - 3 STEP
+
+
+func wordPeckerSplitString(phrase: String, wordCompletion: (String) -> ()) {
+    let words = phrase.split(separator: " ")
+    
+    for word in words {
+        
+        //  word имеет String SubSequence, поэтому внизу приводим его к String
+        for letter in word {
+            print(letter)
+        }
+        wordCompletion(String(word))
+    }
+}
+
+//  В параметр wordCompletion мы вписываем аргумент типа String (имя, которое мы будем использовать внутри блока)
+wordPeckerSplitString(phrase: "Word Pecker") { someWord in
+    print("--> \(someWord.uppercased()) completed")
+}
+
+//  MARK: - 4 STEP
+
+
+//  После того, как наш блок отработает, только после этого ARC может удалять ссылку из памяти.
+func wordPeckerSplitStringDisptchQ(phrase: String, wordCompletion: @escaping (String) -> ()) {
+    let words = phrase.split(separator: " ")
+    
+    DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+        wordCompletion("Finish")
+    }
+    
+    for word in words {
+        
+        for letter in word {
+            print(letter)
+        }
+        wordCompletion(String(word))
+    }
+    
+}
+
+wordPeckerSplitStringDisptchQ(phrase: "Word Pecker") { someWord in
+    print("--> \(someWord.uppercased()) completed")
+}
+//  Finish получим лишь в конце. 
