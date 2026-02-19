899756659fd415818749a871c493dc2f484f47c1
AtesiT <atesit@mail.ru>
2026-02-19 23:31:02 +0300

    ADD: class Vehicle in ClassesAndStructs

---
+++ ClassesAndStructs.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+class Vehicle {
+    let name: String
+    let price: Int
+    let countInStock: Int
+    
+    init(name: String, price: Int, countInStock: Int) {
+        self.name = name
+        self.price = price
+        self.countInStock = countInStock
+    }
+}
+
+
+
