63c909e54402f34153fd66d47bcc3de277187dec
AtesiT <atesit@mail.ru>
2026-03-04 23:22:45 +0300

    INIT: One actor

--- Actors.playground/Contents.swift
+++ Actors.playground/Contents.swift
@@ -27,3 +27,34 @@ account.deposit(amount: 500.0)
 account.withdraw(amount: 200.0)
 print(account.balance)
 account.withdraw(amount: 2000)
+
+//  Actor
+
+actor BankAccountActor {
+    var balance: Double
+    
+    init(balance: Double) {
+        self.balance = balance
+    }
+    
+    func deposit(amount: Double) {
+        balance += amount
+    }
+    
+    func withdraw(amount: Double) {
+        if balance < amount {
+            print("Недостаточно средств")
+            return
+        }
+        balance -= amount
+    }
+}
+
+let accountActor = BankAccountActor(balance: 1000.0)
+
+Task {
+    await accountActor.deposit(amount: 500.0)
+    await accountActor.withdraw(amount: 200.0)
+    await print(accountActor.balance)
+    await accountActor.withdraw(amount: 2000)
+}
